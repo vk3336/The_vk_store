9c4476a80d8a5edfce60824f3fd29038c86e4713
urvikalal <urvi.qodic@gmail.com>
2025-08-29 16:17:44 +0530

    done

--- js/products.js
+++ js/products.js
@@ -1,7 +1,7 @@
 const products = [
     {
         id: 1,
-        name: "Mix chavana12",
+        name: "Mix chavana",
         description: "A perfect mix of sweet and sour flavors with roasted chana and spices. An irresistible snack for any time.",
         image: "images/namkeenproduct/mixchavana.avif",
         price: 250,
